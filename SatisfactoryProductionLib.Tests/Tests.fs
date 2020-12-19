b7dbf99dfe2c6ba9a849ec32e164f1c4d01debaf
Josh Bullock <mail@joshb.me>
2020-12-19 13:01:25 -0500

    Initial commit

---
+++ SatisfactoryProductionLib.Tests/Tests.fs
@@ -0,0 +1,30 @@
+namespace SatisfactoryProductionLib.Tests
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open SatisfactoryProductionLib
+
+[<TestClass>]
+type TestClass () =
+
+    [<TestMethod>]
+    member this.TestMethodPassing () =
+        Assert.IsTrue(true);
+
+    [<TestMethod>]
+    member this.TestEvenSequence() = 
+        let expected = Seq.empty<int> |> Seq.toList
+        let actual = TestMath.squaresOfOdds [2; 4; 6; 8; 10]
+        Assert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestOnesAndEvens() =
+        let expected = [1; 1; 1; 1]
+        let actual = TestMath.squaresOfOdds [2; 1; 4; 1; 6; 1; 8; 1; 10]
+        Assert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestSquaresOfOdds() = 
+        let expected = [1; 9; 25; 49; 81]
+        let actual = TestMath.squaresOfOdds [1; 2; 3; 4; 5; 6; 7; 9; 10]
+        Assert.AreEqual(expected, actual)
---
+++ SatisfactoryProductionLib/Library.fs
@@ -0,0 +1,18 @@
+﻿namespace SatisfactoryProductionLib
+
+module Say =
+
+    let hello name =
+        printfn "Hello %s" name
+
+module TestMath = 
+
+    let private isOdd x = x % 2 <> 0
+    let private square x = x * x
+
+    let squaresOfOdds xs =
+        xs
+        |> Seq.filter isOdd 
+        |> Seq.map square
+        |> Seq.toList
+
