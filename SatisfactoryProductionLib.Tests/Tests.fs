f8961d2afd3c0bb5425d46822ec5f5f5051c8348
Josh Bullock <mail@joshb.me>
2020-12-19 15:07:17 -0500

    Create determineRecipeDependencies

--- SatisfactoryProductionLib.Tests/Tests.fs
+++ SatisfactoryProductionLib.Tests/Tests.fs
@@ -8,10 +8,6 @@ open SatisfactoryProductionLib
 type TestClass () =
 
     [<TestMethod>]
-    member this.TestMethodPassing () =
-        Assert.IsTrue(true);
-
-    [<TestMethod>]
     member this.TestEvenSequence() = 
         let expected = Seq.empty<int> |> Seq.toList
         let actual = TestMath.squaresOfOdds [2; 4; 6; 8; 10]
@@ -27,4 +23,13 @@ type TestClass () =
     member this.TestSquaresOfOdds() = 
         let expected = [1; 9; 25; 49; 81]
         let actual = TestMath.squaresOfOdds [1; 2; 3; 4; 5; 6; 7; 9; 10]
+        Assert.AreEqual(expected, actual)
+
+[<TestClass>]
+type ProductionTestClass () =
+
+    [<TestMethod>]
+    member this.TestGetRecipeDependencies() =
+        let expected : list<Production.MaterialDependency> = [ { material = Materials.IRON_ORE; amount = 10 } ]
+        let actual = Production.determineRecipeDependencies Production.ironIngotRecipe 10
         Assert.AreEqual(expected, actual)
---
+++ SatisfactoryProductionLib/Production.fs
@@ -0,0 +1,42 @@
+﻿namespace SatisfactoryProductionLib
+
+module Materials =
+    
+    let IRON_ORE = "IRON_ORE"
+    let IRON_INGOT = "IRON_INGOT"
+    let IRON_PLATE = "IRON_PLATE"
+
+    type Material = 
+        { id: string
+          name: string
+          isNaturallyOcurring: bool }
+
+    let ironOreMaterial = 
+        { id = IRON_ORE
+          name = "Iron Ore"
+          isNaturallyOcurring = true }
+
+    let ironIngotMaterial = 
+        { id = IRON_INGOT
+          name = "Iron Ingot"
+          isNaturallyOcurring = false }
+
+module Production =
+
+    type MaterialDependency = { material: string; amount: int }
+
+    type MaterialRecipe = 
+        { outputMaterial: string
+          outputCount: int
+          materialDependencies: list<MaterialDependency> }
+
+    let ironIngotRecipe = {
+        outputMaterial = Materials.IRON_INGOT
+        outputCount = 1
+        materialDependencies = [ { material = Materials.IRON_ORE; amount = 1  } ]
+    }
+
+    let determineRecipeDependencies (recipe: MaterialRecipe) amount = 
+        recipe.materialDependencies
+        |> List.map ( fun dependency -> { dependency with amount = dependency.amount * amount } ) 
+        
