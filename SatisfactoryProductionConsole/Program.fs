3b406f73b3bc4bc4dc8b307f524733e606afc55a
Josh Bullock <mail@joshb.me>
2020-12-19 17:37:38 -0500

    Print results from console

--- SatisfactoryProductionConsole/Program.fs
+++ SatisfactoryProductionConsole/Program.fs
@@ -1,8 +1,14 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open SatisfactoryProductionLib
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    printfn "Satisfactory Production Target Calculator"
+
+    let result = Production.determineRecipeDependencies MaterialRecipes.reinforcedIronPlate 1.0
+
+    printfn "%A" result
+
     0 // return an integer exit code
